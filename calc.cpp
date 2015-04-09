b25a0632ac768bfa245c18274e905e454ad96804
Raneem_medhat <raneem.medhat@gmail.com>
2015-04-09 11:40:40 +0200

    Fixing bugs

--- calc.cpp
+++ calc.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
    float a, b, result;
-   char operation;
+   char operation= null;
  
    // Get numbers and mathematical operator from user input
    cin >> a >> operation >> b;
