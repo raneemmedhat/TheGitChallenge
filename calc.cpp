8b4d36314bbfd22ea3eece235a548d1ea9c0ad4c
Raneem_medhat <raneem.medhat@gmail.com>
2015-04-09 11:36:22 +0200

    this is the first blue node

---
+++ calc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   float a, b, result;
+   char operation;
+ 
+   // Get numbers and mathematical operator from user input
+   cin >> a >> operation >> b;
+   return 0;
+}
