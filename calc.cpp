5bafbfe2c63d2dae44ff16b4a225d6f145c0b0ca
Raneem_medhat <raneem.medhat@gmail.com>
2015-04-09 11:47:17 +0200

    Start developing- node1 yellow

--- calc.cpp
+++ calc.cpp
@@ -5,7 +5,7 @@ int main()
 {
    float a, b, result;
    char operation;
- 
+ result = a + b;
    // Get numbers and mathematical operator from user input
    cin >> a >> operation >> b;
    return 0;
