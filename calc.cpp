f06422642791f704989858aa2a7d98f686687ee6
Raneem_medhat <raneem.medhat@gmail.com>
2015-04-09 11:48:40 +0200

    second yellow node

--- calc.cpp
+++ calc.cpp
@@ -5,7 +5,12 @@ int main()
 {
    float a, b, result;
    char operation;
- result = a + b;
+   switch(operation)
+   {
+   case '+':
+         result = a + b;
+         break;
+}
    // Get numbers and mathematical operator from user input
    cin >> a >> operation >> b;
    return 0;
